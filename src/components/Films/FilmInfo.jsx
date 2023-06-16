b442496109fab91ecb673ad44e49e2a297f6e7d4
Lazu_ve01@student.itstep.org <Lazu_ve01@student.itstep.org>
2023-06-16 15:04:08 +0100

    little changes

--- src/components/Films/FilmInfo.jsx
+++ src/components/Films/FilmInfo.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const FilmInfo = ({item}) => {
     return (
-        <div className='film-poster'>
-            <img className='film-img' src={item.image} alt={item.name} />
-            <div className='film-line'></div>
-            <div className='film-info'>
-                <p className='filmName'>{item.name}</p>
-                <p className='filmYear'>{item.year}</p>
+        <div className="col-sm-4">
+            <div className='film-poster'>
+                <img className='film-img' src={item.image} alt={item.name} />
+                <div className='film-line'></div>
+                <div className='film-info'>
+                    <p className='filmName'>{item.name}</p>
+                    <p className='filmYear'>{item.year}</p>
+                </div>
             </div>
         </div>
     );
--- src/components/Films/FilmItems.jsx
+++ src/components/Films/FilmItems.jsx
@@ -3,25 +3,12 @@ import FilmInfo from './FilmInfo';
 
 const FilmItems = ({ data }) => {
 
-    const itemsPerColumn = Math.ceil(data.length / 3);
-    const columns = [];
-
-    for (let i = 0; i < 3; i++) {
-        const columnItems = data.slice(i * itemsPerColumn, (i + 1) * itemsPerColumn);
-
-        columns.push(
-        <div key={i} className="col-sm-4">
-            {columnItems.map((item) => (
-                <FilmInfo item={item} />
-            ))}
-        </div>
-        );
-    }
-
     return (
         <div>
             <div className='row'>
-                {columns}  
+                {data.map((item) => (
+                    <FilmInfo item={item} />
+                ))}
             </div>
         </div>
     );
